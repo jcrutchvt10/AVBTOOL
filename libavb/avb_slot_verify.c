bc41cead048a90f63e3dd4335097c5588ec09345
David Zeuthen <zeuthen@google.com>
2017-02-16 14:07:11 -0500

    libavb: Include androidboot.vbmeta.version in the generated command-line.
    This version number represents the version of the libavb embedded in
    the bootloader. This can be used at run-time to reject updates that
    would write a vbmeta partition with a newer version than what the
    bootloader supports.
    
    (Also include some fixes for the previous CL which didn't make the cut
    before Treehugger merged it.)
    
    Bug: 35416772
    Test: New unit tests and all unit tests pass.
    Test: Manually tested on UEFI based bootloader.
    Change-Id: Ie6aea68d0d5154be64cb1fff0699d16a0b123d39

--- libavb/avb_slot_verify.c
+++ libavb/avb_slot_verify.c
@@ -786,15 +786,17 @@ static int cmdline_append_option(AvbSlotVerifyData* slot_data,
   return 1;
 }
 
-static int cmdline_append_uint64_base10(AvbSlotVerifyData* slot_data,
-                                        const char* key,
-                                        uint64_t value) {
-  const int MAX_DIGITS = 32;
-  char rev_digits[MAX_DIGITS];
-  char digits[MAX_DIGITS];
+#define AVB_MAX_DIGITS_UINT64 32
+
+/* Writes |value| to |digits| in base 10 followed by a NUL byte.
+ * Returns number of characters written excluding the NUL byte.
+ */
+static size_t uint64_to_base10(uint64_t value,
+                               char digits[AVB_MAX_DIGITS_UINT64]) {
+  char rev_digits[AVB_MAX_DIGITS_UINT64];
   size_t n, num_digits;
 
-  for (num_digits = 0; num_digits < MAX_DIGITS - 1;) {
+  for (num_digits = 0; num_digits < AVB_MAX_DIGITS_UINT64 - 1;) {
     rev_digits[num_digits++] = (value % 10) + '0';
     value /= 10;
     if (value == 0) {
@@ -806,7 +808,33 @@ static int cmdline_append_uint64_base10(AvbSlotVerifyData* slot_data,
     digits[n] = rev_digits[num_digits - 1 - n];
   }
   digits[n] = '\0';
+  return n;
+}
 
+static int cmdline_append_version(AvbSlotVerifyData* slot_data,
+                                  const char* key,
+                                  uint64_t major_version,
+                                  uint64_t minor_version) {
+  char major_digits[AVB_MAX_DIGITS_UINT64];
+  char minor_digits[AVB_MAX_DIGITS_UINT64];
+  char combined[AVB_MAX_DIGITS_UINT64 * 2 + 1];
+  size_t num_major_digits, num_minor_digits;
+
+  num_major_digits = uint64_to_base10(major_version, major_digits);
+  num_minor_digits = uint64_to_base10(minor_version, minor_digits);
+  avb_memcpy(combined, major_digits, num_major_digits);
+  combined[num_major_digits] = '.';
+  avb_memcpy(combined + num_major_digits + 1, minor_digits, num_minor_digits);
+  combined[num_major_digits + 1 + num_minor_digits] = '\0';
+
+  return cmdline_append_option(slot_data, key, combined);
+}
+
+static int cmdline_append_uint64_base10(AvbSlotVerifyData* slot_data,
+                                        const char* key,
+                                        uint64_t value) {
+  char digits[AVB_MAX_DIGITS_UINT64];
+  uint64_to_base10(value, digits);
   return cmdline_append_option(slot_data, key, digits);
 }
 
@@ -896,6 +924,15 @@ AvbSlotVerifyResult avb_slot_verify(AvbOps* ops,
     if (!cmdline_append_option(slot_data,
                                "androidboot.vbmeta.device",
                                "PARTUUID=$(ANDROID_VBMETA_PARTUUID)")) {
+      ret = AVB_SLOT_VERIFY_RESULT_ERROR_OOM;
+      goto fail;
+    }
+
+    /* Add androidboot.vbmeta.version option. */
+    if (!cmdline_append_version(slot_data,
+                                "androidboot.vbmeta.version",
+                                AVB_MAJOR_VERSION,
+                                AVB_MINOR_VERSION)) {
       ret = AVB_SLOT_VERIFY_RESULT_ERROR_OOM;
       goto fail;
     }
